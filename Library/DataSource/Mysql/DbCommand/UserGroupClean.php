af8daf499da148fbabf2d9e508c60782b55d6300
viewfuhrer <atlantij@gmail.com>
2012-05-11 16:43:05 +0400

    FIX Автозаполнялки снова снова полностью готовы

--- Library/DataSource/Mysql/DbCommand/UserGroupClean.php
+++ Library/DataSource/Mysql/DbCommand/UserGroupClean.php
@@ -6,6 +6,6 @@ class UserGroupClean extends DbCommand
 {
     public function UserGroupClean($iGroupId)
     {
-        return $this->_connection->delete("delivery_user_to_group", "group_id = {$iGroupId} AND status = 1");
+        return $this->_connection->delete("delivery_user_to_group", "group_id = {$iGroupId} AND status = '1'");
     }
 }
